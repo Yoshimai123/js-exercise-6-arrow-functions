2506c183b0438ae193cd2571bd16bb2e1acf250f
Vitor Duarte <duartevitor178@gmail.com>
2023-11-07 10:04:39 +0100

    feat | ex 3

--- arrow-subtract.js
+++ arrow-subtract.js
@@ -4,6 +4,6 @@ window.addEventListener("load", function(){
     const subtractA = Number(prompt("Select a number"));
     const subtractB = Number(prompt("Select another number"))
     let subtractTotal = subtract(subtractA, subtractB);
-    document.body.innerHTML += `${subtractA} - ${subtractB} =` + subtractTotal;
+    document.body.innerHTML += `${subtractA} - ${subtractB} = ${subtractTotal}`;
     document.body.style.fontSize = "100px"
 })
--- simple-html.js
+++ simple-html.js
@@ -1,3 +1,8 @@
-window.addEventListener("load" function(){
-    this.document.getElementsByTagName("div").innerHTML
+window.addEventListener("load", function(){
+    let divElement = document.getElementById("div");
+    let textStrong = false;
+    divElement.innerHTML = "<strong>This is my text</strong>";
+    divElement.onclick = function(){
+        divElement.innerText = "This is my new text";
+    };
 })
